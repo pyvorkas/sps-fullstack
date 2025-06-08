3bc05ea388d1462b7ef1c8d7fc351dbaf4ea93c6
pyvorkas <martas.ryvora@gmail.com>
2025-06-08 15:07:44 +0200

    fix: úprava databázových skriptů

--- database/db.js
+++ database/db.js
@@ -1,13 +1,35 @@
 const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'));
+// Zajistíme, že adresář database existuje
+const dbDir = path.join(__dirname);
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
 
-const schema = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
-db.exec(schema, (err) => {
-  if (err) console.error('Error initializing schema:', err.message);
-  else console.log('Database schema loaded.');
-});
+const dbPath = path.join(__dirname, 'database.sqlite');
+const db = new sqlite3.Database(dbPath);
 
-module.exports = db;
+// Inicializace databáze
+function initializeDatabase() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, 'schema.sql'), 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      
+      db.exec(data, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        console.log('Databáze byla úspěšně inicializována');
+        resolve();
+      });
+    });
+  });
+}
+
+// Správný export obou - db i initializeDatabase
+module.exports = { db, initializeDatabase };
+
